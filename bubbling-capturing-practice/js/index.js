10ed01f2ec7fd7120fdbc8b77d91860be8c60bbe
Viktoriia <pecourses>
2022-12-03 18:31:26 +0200

    Add burger script

--- bubbling-capturing-practice/js/index.js
+++ bubbling-capturing-practice/js/index.js
@@ -2,16 +2,30 @@
 
 // приклад роботи з classList
 // classlist - рядок, в якому класи роздылено пробылами (як в html)
-const h1El = document.querySelector('h1');
-h1El.classList.add('classH1');
-h1El.classList.remove('classH1');
+// const h1El = document.querySelector('h1');
+// h1El.classList.add('classH1');
+// h1El.classList.remove('classH1');
 
-const burgerBtn = document.querySelector('.burgerMenuBtn');
+// const burgerBtn = document.querySelector('.burgerMenuBtn');
 
-burgerBtn.onclick = () => {
-  h1El.classList.toggle('classH1');
-};
+// burgerBtn.onclick = () => {
+//   h1El.classList.toggle('classH1');
+// };
 
 // при кліку на кнопку змінювати клас на burgerMenuContainerActive
 
 // при кліку на body знімювати на burgerMenuContainerHide
+
+const burgerBtn = document.querySelector('.burgerMenuBtn');
+const burgerContainer = document.querySelector('.burgerMenuContainer');
+
+burgerBtn.addEventListener('click', e => {
+  burgerContainer.classList.toggle('burgerMenuContainerActive');
+  burgerContainer.classList.toggle('burgerMenuContainerHide');
+  e.stopPropagation();
+});
+
+document.body.addEventListener('click', () => {
+  burgerContainer.classList.remove('burgerMenuContainerActive');
+  burgerContainer.classList.add('burgerMenuContainerHide');
+});
